06ceda7c8072d39b875981649b262c34b41c5739
lupheron <nexcode17@gmail.com>
2026-01-29 23:52:36 +0500

    dynamic endpoints

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
     @Get()
     findAll() {
         return "All properties";
+    }
+
+    @Get(":id/:slug")
+    findOne(@Param("id") id: string, @Param("slug") slug: string) {
+        return "Find one property" + id;
     }
 
     @Post() 
